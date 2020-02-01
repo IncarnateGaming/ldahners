a4d46407b4baf08843712d5751f2108b44ad779a
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-01-31 19:10:35 -0500

    quick patch

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @IsGranted("ROLE_ADMIN")
