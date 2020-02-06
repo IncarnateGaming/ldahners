ac933b0cd27bdee16de9d5390ec1ba845c8b066e
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-02-05 19:38:26 -0500

    turned security back on

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -26,8 +26,8 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 /**
+ * @IsGranted("ROLE_ADMIN")
  */
-//* @IsGranted("ROLE_ADMIN")
 class AdminController extends AbstractController
 {
     /**
